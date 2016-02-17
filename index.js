760625c76dbfe56764b3203a070ebae26873a334
Yunfeng Zhang <zhangyun@us.ibm.com>
2016-02-17 14:01:10 -0500

    Remove lame dependency. Pass the correct parameters to sox and arecord for Watson STT.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var isMacOrWin = require('os').type() == 'Darwin' || require('os').type().indexOf('Windows') > -1;
 var spawn = require('child_process').spawn
 var PassThrough = require('stream').PassThrough;
-var lame = require('lame');
 
 var ps = null;
 
@@ -13,25 +12,11 @@ var start = function(options) {
     
     if(ps == null) {
         ps = isMacOrWin
-        ? spawn('sox', ['-d', '-t', 'dat', '-p'])
-        : spawn('arecord', ['-D', 'plughw:1,0', '-f', 'dat']);
+        ? spawn('sox', ['-d', '-t', 's16', '-r', '44100', '-c', '2', '-'])
+        : spawn('arecord', ['-D', 'plughw:0,0', '-f', 'dat']);
 
-        if(options.mp3output === true) {
-            var encoder = new lame.Encoder( {
-                channels: 2,
-                bitDepth: 16,
-                sampleRate: 44100
-            });
-
-            ps.stdout.pipe(encoder);
-            encoder.pipe(audio);
-            ps.stderr.pipe(info);
-
-        } else {
-            ps.stdout.pipe(audio);
-            ps.stderr.pipe(info);
-
-        }
+        ps.stdout.pipe(audio);
+        ps.stderr.pipe(info);
     }
 };
 
